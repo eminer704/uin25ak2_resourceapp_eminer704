953328c1af221e5cdd3fc5001773176ebad6e409
eminer704 <>
2025-03-17 13:49:13 +0100

    update-main

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-//Oppretter en React-rot i HTML-elementet med id "root"
+//Oppretter en react-rot i HTML-elementet med id "root"
 createRoot(document.getElementById('root')).render(
 
   //Gjengir hovedkomponenten (App) i root-elementet
